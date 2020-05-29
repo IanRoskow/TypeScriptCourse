e88bb56401a5d95446bdc3190c7cf45f3d4118ff
Ian Roskow <ianroskow@gmail.com>
2020-05-29 15:56:33 +0200

    Finished up section 5 over mastering typed arrays

---
+++ features/arrays.ts
@@ -0,0 +1,24 @@
+const carMakers = ['ford', 'toyota', 'chevy'];
+const carSellers: string[] = [];
+
+const dates = [new Date(), new Date()];
+
+const carsByMake: string[][] = [['f150'], ['corolla'], ['camaro']];
+
+//Help with inference when extracting values
+const car = carMakers[0];
+const myCar = carMakers.pop();
+
+//Prevent incompatible values
+carMakers.push(100);
+
+//Hepl with 'map'
+carMakers.map((car: string): string => {
+  //Get auto complete on value being returned
+  return car.toUpperCase();
+});
+
+//Flexible Types
+const importantDates: (Date | string)[] = [new Date(), '2030-10-10'];
+importantDates.push('2030-10-10');
+importantDates.push(new Date());
