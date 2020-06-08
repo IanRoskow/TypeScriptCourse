be3aa39e7e88e8e14fee294d97a98cd6d5ba9299
Ian Roskow <ianroskow@gmail.com>
2020-06-08 23:37:53 +0200

    Added type guards for bad approach

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,14 +1,20 @@
 class Sorter {
-  constructor(public collection: number[]) {}
+  constructor(public collection: number[] | string) {}
 
   sort(): void {
     const { length } = this.collection;
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection[j] > this.collection[j + 1]) {
-          const temp = this.collection[j];
-          this.collection[j] = this.collection[j + 1];
-          this.collection[j + 1] = temp;
+        if (this.collection instanceof Array) {
+          if (this.collection[j] > this.collection[j + 1]) {
+            const temp = this.collection[j];
+            this.collection[j] = this.collection[j + 1];
+            this.collection[j + 1] = temp;
+          }
+        }
+        //comparison for a string
+        //can only use this type guard for number string or boolean
+        if (typeof this.collection === 'string') {
         }
       }
     }
