ed527754cc956b5b891a5a8a60ab9b9aea4335b7
Ian Roskow <ianroskow@gmail.com>
2020-06-11 12:05:38 +0200

    Added implemantation interface to numbers collection

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,7 +1,7 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 
-const numbersCollection = new NumbersCollection([10000, 3, -5, 0]);
+const numbersCollection = new NumbersCollection([23, 50, -5, 0]);
 const sorter = new Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);
