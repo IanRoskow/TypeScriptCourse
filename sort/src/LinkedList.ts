e7ea63a2130111670744c2fcedef3696640b25c4
Ian Roskow <ianroskow@gmail.com>
2020-06-17 12:13:10 +0200

    Completed sorter collections

---
+++ sort/src/LinkedList.ts
@@ -0,0 +1,89 @@
+class Node {
+  next: Node | null = null;
+  constructor(public data: number) {}
+}
+
+import { Sortable } from './Sorter';
+
+export class LinkedList implements Sortable {
+  head: Node | null = null;
+
+  add(data: number): void {
+    const node = new Node(data);
+
+    if (!this.head) {
+      this.head = node;
+      return;
+    }
+
+    let currentNode = this.head;
+    while (currentNode.next) {
+      currentNode = currentNode.next;
+    }
+    currentNode.next = node;
+  }
+
+  get length(): number {
+    let length = 0;
+
+    if (!this.head) {
+      return length;
+    }
+
+    let currentNode = this.head;
+    length++;
+    while (currentNode.next) {
+      currentNode = currentNode.next;
+      length++;
+    }
+
+    return length;
+  }
+
+  at(index: number): Node {
+    if (!this.head) {
+      throw new Error('Index out of bounds');
+    }
+
+    let counter = 0;
+    let node: Node | null = this.head;
+    while (node) {
+      if (counter === index) {
+        return node;
+      }
+      counter++;
+      node = node.next;
+    }
+
+    throw new Error('Index out of bounds');
+  }
+
+  compare(leftIndex: number, rightIndex: number): boolean {
+    if (!this.head) {
+      throw new Error('List is empty');
+    }
+
+    return this.at(leftIndex).data > this.at(rightIndex).data;
+  }
+
+  swap(leftIndex: number, rightIndex: number): void {
+    const leftNode = this.at(leftIndex);
+    const rightNode = this.at(rightIndex);
+
+    const leftHand = leftNode.data;
+    leftNode.data = rightNode.data;
+    rightNode.data = leftHand;
+  }
+
+  print(): void {
+    if (!this.head) {
+      return;
+    }
+
+    let node: Node | null = this.head;
+    while (node) {
+      console.log(node.data);
+      node = node.next;
+    }
+  }
+}
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,9 +1,21 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
-const numbersCollection = new NumbersCollection([23, 50, -5, 0]);
-const characterCollection = new CharactersCollection('XnxadsAb');
-const sorter = new Sorter(characterCollection);
+// const numbersCollection = new NumbersCollection([23, 50, -5, 0]);
+// const characterCollection = new CharactersCollection('XnxadsAb');
+// const sorter = new Sorter(characterCollection);
+// sorter.sort();
+// console.log(characterCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+const sorter = new Sorter(linkedList);
 sorter.sort();
-console.log(characterCollection.data);
+linkedList.print();
+``;
