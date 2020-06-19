609cfb24d9ac4409738a7a4a1c7ffb428f517da7
Ian Roskow <ianroskow@gmail.com>
2020-06-19 18:13:52 +0200

    Implemented custom reader class

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,29 +1,23 @@
-import fs from 'fs';
+import { CsvFileReader } from './CsvFileReader';
 
-const matches = fs
-  .readFileSync('original.csv', {
-    encoding: 'utf-8',
-  })
-  .split('\n')
-  .map((row: string): string[] => {
-    return row.split(',');
-  });
+const reader = new CsvFileReader('original.csv');
+reader.read();
 
 //enum - enumeration
 enum MatchResult {
-  HomeWin = 'H',
-  AwayWin = 'A',
-  Draw = 'D',
+    HomeWin = 'H',
+    AwayWin = 'A',
+    Draw = 'D',
 }
 
 let manUnitedWins = 0;
 
-for (let match of matches) {
-  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
-    manUnitedWins++;
-  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
-    manUnitedWins++;
-  }
+for (let match of reader.data) {
+    if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+        manUnitedWins++;
+    } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+        manUnitedWins++;
+    }
 }
 
 console.log(manUnitedWins);
