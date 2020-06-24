5c6957e314cf8a081b959bd587eb4be218851ba9
Ian Roskow <ianroskow@gmail.com>
2020-06-24 15:48:09 +0200

    Refactored code to be composition based

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,20 +1,17 @@
 import { CsvFileReader } from './CsvFileReader';
 import { MatchReader } from './MatchReader';
-import { MatchResult } from './MatchResult';
+import { Summary } from './Summary';
+import { ConsoleReport } from './reportTargets/ConsoleReport';
+import { WinsAnalysis } from './analyzers/WinsAnalysis';
 
 const csvFileReader = new CsvFileReader('original.csv');
 
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
-let manUnitedWins = 0;
+const summary = new Summary(
+    new WinsAnalysis('Man United'),
+    new ConsoleReport()
+);
 
-for (let match of matchReader.matches) {
-    if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
-        manUnitedWins++;
-    } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
-        manUnitedWins++;
-    }
-}
-
-console.log(manUnitedWins);
+summary.buildAndPrintReport(matchReader.matches);
