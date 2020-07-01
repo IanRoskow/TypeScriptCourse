1533421c13020e62626b14a6e0d64ba4b11ad1f4
Ian Roskow <ianroskow@gmail.com>
2020-07-01 13:53:54 +0200

    Added set method to user class

--- web/src/index.ts
+++ web/src/index.ts
@@ -2,4 +2,6 @@ import { User } from './models/User';
 
 const user = new User({ name: 'myname', age: 20 });
 
+user.set({ name: 'Ian', age: 999 });
+
 console.log(user.get('name'), user.get('age'));
