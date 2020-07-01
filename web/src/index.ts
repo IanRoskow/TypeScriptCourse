5c744452e308742217a15f9ac4a6387603feb790
Ian Roskow <ianroskow@gmail.com>
2020-07-01 14:39:44 +0200

    Completed event system for User Class

--- web/src/index.ts
+++ web/src/index.ts
@@ -2,6 +2,14 @@ import { User } from './models/User';
 
 const user = new User({ name: 'myname', age: 20 });
 
-user.set({ name: 'Ian', age: 999 });
+user.on('change', () => {
+    console.log('Change #1');
+});
+user.on('change', () => {
+    console.log('Change #2');
+});
+user.on('save', () => {
+    console.log('Save #1');
+});
 
-console.log(user.get('name'), user.get('age'));
+user.trigger('click');
