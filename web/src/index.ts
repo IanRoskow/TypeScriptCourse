1fbff70b3d4119d54f23ae8889130161a9f792e1
Ian Roskow <ianroskow@gmail.com>
2020-07-06 09:24:11 +0200

    Added pass through classes for user class

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,9 +1,9 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'thomas', age: 15 });
+const user = new User({ id: 1, name: 'Ian Gons', age: 50 });
 
-user.events.on('change', () => {
-    console.log('change');
+user.on('save', () => {
+    console.log(user);
 });
 
-user.events.trigger('change');
+user.save();
