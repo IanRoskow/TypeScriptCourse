6fdf429d268e717f30c03efe3173d96b9d685dce
Ian Roskow <ianroskow@gmail.com>
2020-07-08 18:23:18 +0200

    Final implementation before typescript refactor

--- server/build/routes/loginRoutes.js
+++ server/build/routes/loginRoutes.js
@@ -2,6 +2,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.router = void 0;
 var express_1 = require("express");
+function requireAuth(req, res, next) {
+    if (req.session && req.session.loggedIn) {
+        next();
+        return;
+    }
+    res.status(403);
+    res.send('Not permitted');
+}
 var router = express_1.Router();
 exports.router = router;
 router.get('/login', function (req, res) {
@@ -16,4 +24,19 @@ router.post('/login', function (req, res) {
     else {
         res.send('Invalid email or password');
     }
+});
+router.get('/', function (req, res) {
+    if (req.session && req.session.loggedIn) {
+        res.send("\n        <div>\n            <div>You are logged in</div>\n            <a href='/logout'>Logout</a>\n        </div>\n        ");
+    }
+    else {
+        res.send("\n        <div>\n            <div>You are not logged in</div>\n            <a href='/login'>Login</a>\n        </div>\n        ");
+    }
+});
+router.get('/logout', function (req, res) {
+    req.session = { loggedIn: false };
+    res.redirect('/');
+});
+router.get('/protected', requireAuth, function (req, res) {
+    res.send('Welcome to the protected route, logged in user');
 });
