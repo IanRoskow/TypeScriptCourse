a38d49daafe91be0b2383686553935cda1d12928
Ian Roskow <ianroskow@gmail.com>
2020-08-01 10:59:23 +0200

    Wrapped up features for metadata

--- features/metadata.ts
+++ features/metadata.ts
@@ -1,17 +1,41 @@
 import 'reflect-metadata';
 
-const plane = {
-    color: 'red',
-};
+@controller
+class Plane {
+    color: string = 'red';
 
-Reflect.defineMetadata('note', 'bye there', plane);
-Reflect.defineMetadata('note', 'hi there', plane, 'color');
+    @get('/login')
+    fly(): void {
+        console.log('vrrrrr');
+    }
+}
+function get(path: string) {
+    return function (target: Plane, key: string) {
+        Reflect.defineMetadata('path', path, target, key);
+    };
+}
 
-const note = Reflect.getMetadata('note', plane);
-const colornote = Reflect.getMetadata('note', plane, 'color');
+function controller(target: typeof Plane) {
+    for (let key in target.prototype) {
+        const path = Reflect.getMetadata('path', target.prototype, key);
+        //How you can implement router and middleware
+        //const middleware = Reflect.getMetadata('middleware', target.prototype)
 
-console.log(colornote, note);
+        //router.get(path, target.prototype[key]);
+    }
+}
 
+// const plane = {
+//     color: 'red',
+// };
+
+// Reflect.defineMetadata('note', 'bye there', plane);
+// Reflect.defineMetadata('note', 'hi there', plane, 'color');
+
+// const note = Reflect.getMetadata('note', plane);
+// const colornote = Reflect.getMetadata('note', plane, 'color');
+
+// console.log(colornote, note);
 // Reflect.defineMetadata('note', 'hi there', plane);
 // Reflect.defineMetadata('height', 10, plane);
 
