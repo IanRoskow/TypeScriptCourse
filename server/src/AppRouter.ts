3f62560373d5edb8278ddde9f55bff1da8fd0abf
Ian Roskow <ianroskow@gmail.com>
2020-08-03 10:07:26 +0200

    Converted app router to singleton

---
+++ server/src/AppRouter.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+
+//Singleton Router
+//Only one used in the whole app
+export class AppRouter {
+    private static instance: express.Router;
+
+    static getInstance(): express.Router {
+        if (!AppRouter.instance) {
+            AppRouter.instance = express.Router();
+        }
+
+        return AppRouter.instance;
+    }
+}
