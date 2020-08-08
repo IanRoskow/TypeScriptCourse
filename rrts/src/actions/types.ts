dd5116daa312d0f79f9c5fcb3d7cfb116aab7805
Ian Roskow <ianroskow@gmail.com>
2020-08-08 22:24:34 +0200

    Fixed type annotation

--- rrts/src/actions/types.ts
+++ rrts/src/actions/types.ts
@@ -1,6 +1,10 @@
+import { FetchTodosAction, DeleteTodoAction } from './todos';
+
 //Don't need to define the right hand side because TS enum
 //automatically gives it a unique integer
 export enum ActionTypes {
     fetchTodos,
     deleTodo,
 }
+
+export type Action = FetchTodosAction | DeleteTodoAction;
